dbbafe22bc82fa2647d5f093231cbb50044f96b8
paul <paulcynic@gmail.com>
2019-09-06 16:53:35 +0800

    theme and other aes settings

--- data-ggplot-evolution-vol3.r
+++ data-ggplot-evolution-vol3.r
@@ -3,8 +3,21 @@
 df_sorted <- df_ratios %>%
   mutate(region = fct_reorder(region, -student_ratio_region))
 
-ggplot(df_sorted, aes(region, student_ratio))+
+theme_set(theme_light(base_size = 15, base_family = "Poppins"))
+
+ggplot(df_sorted, aes(region, student_ratio, color = region))+
   geom_boxplot()+
-  scale_y_continuous(limits = c(0, 90))+ #even if there was a coord_flip it was still the scale_y not x
-  coord_flip()
+  coord_flip()+
+  scale_y_continuous(limits = c(0, 90), expand = c(0.005, 0.005))+ #even if there was a coord_flip it was still the scale_y not x
+  scale_color_uchicago()+
+  labs(x = NULL, y = "Student Teacher Ratio")+
+  theme(legend.position = "none",
+        panel.grid = element_blank(),
+        axis.title = element_text(size = 12),
+        axis.text.x = element_text(family = "Roboto Mono", size = 10)
+        )
+
+
+
+
 
