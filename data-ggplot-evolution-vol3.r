accd7c248999de0491a6622987c0c449fac8195c
paul <paulcynic@gmail.com>
2019-09-06 16:38:37 +0800

    coord_flip and limits to increase readability

--- data-ggplot-evolution-vol3.r
+++ data-ggplot-evolution-vol3.r
@@ -4,4 +4,7 @@ df_sorted <- df_ratios %>%
   mutate(region = fct_reorder(region, -student_ratio_region))
 
 ggplot(df_sorted, aes(region, student_ratio))+
-  geom_boxplot()
+  geom_boxplot()+
+  scale_y_continuous(limits = c(0, 90))+ #even if there was a coord_flip it was still the scale_y not x
+  coord_flip()
+
