847268cca93bf6056a7c493c3fa8e39973025ad1
paul <paulcynic@gmail.com>
2019-09-06 16:35:37 +0800

    sorted boxplot

--- data-ggplot-evolution-vol3.r
+++ data-ggplot-evolution-vol3.r
@@ -1,6 +1,7 @@
 
 
+df_sorted <- df_ratios %>%
+  mutate(region = fct_reorder(region, -student_ratio_region))
 
-
-ggplot(df_ratios, aes(region, student_ratio))+
+ggplot(df_sorted, aes(region, student_ratio))+
   geom_boxplot()
